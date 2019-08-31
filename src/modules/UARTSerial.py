6cacd3650f84617a1d1a052a5004469385f039a6
alvaromfcunha <alvaro99.cunha@gmail.com>
2019-08-31 11:20:12 -0300

    fixed dumb self error

--- src/modules/UARTSerial.py
+++ src/modules/UARTSerial.py
@@ -3,10 +3,10 @@ import serial
 class Serial:
     def __init__(self, port, baud_rate):
         self.ser = serial.Serial(port, baud_rate)
-        ser.reset_input_buffer()
+        self.ser.reset_input_buffer()
     def read(self):
-        ser.reset_input_buffer()
+        self.ser.reset_input_buffer()
         return self.ser.read_until()
     def write(self, string):
-        ser.reset_input_buffer()
+        self.ser.reset_input_buffer()
         return self.ser.write(string)
