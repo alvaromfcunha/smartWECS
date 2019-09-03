71870e005b02cc4afe4d9e041d9f05481e80123c
alvaromfcunha <alvaro99.cunha@gmail.com>
2019-09-03 10:53:41 -0300

    serial timeout

--- src/rbpi/modules/UARTSerial.py
+++ src/rbpi/modules/UARTSerial.py
@@ -4,6 +4,7 @@ class Serial:
     def __init__(self, port, baud_rate):
         self.ser = serial.Serial(port, baud_rate)
         self.ser.reset_input_buffer()
+        self.ser.timeout = 1
     def read(self):
         self.ser.reset_input_buffer()
         return self.ser.readline()
