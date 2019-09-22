8a57b12e0393fe8f17265681ad123c39bae1e436
alvaromfcunha <alvaro99.cunha@gmail.com>
2019-09-22 11:04:15 -0300

    processed data from sensors

--- src/rbpi/main.py
+++ src/rbpi/main.py
@@ -1,38 +1,67 @@
 import time
+import threading
+from timeloop import Timeloop
+from datetime import timedelta
 from modules.AWSServer import MQTTServer
 from modules.UARTSerial import Serial
-   
+
+currentAdc = None
+waterPeriod = None
+
+electricity = 0
+water = 0
+
+tl = Timeloop()
+
+server = MQTTServer("a2cr09xzlv9lkb-ats.iot.us-west-2.amazonaws.com", "resources/root-CA.crt",
+                         "resources/cert.key", "resources/cert.crt", "")
+server.connect()
+
 def main():
 
+    global currentAdc
+    global waterPeriod
+
     serial = Serial("/dev/ttyS0", 9600, 5)
-    server = MQTTServer("a2cr09xzlv9lkb-ats.iot.us-west-2.amazonaws.com", "resources/root-CA.crt",
-                         "resources/cert.key", "resources/cert.crt", "")
-    server.connect() 
+
+    tl.start(block=False)
 
     while(1):
+        
         serial.write('e'.encode())
-        eletricity = serial.read()
+        currentAdc = serial.read()
 
-        print("ELE:", eletricity)
+        print("ELE:", currentAdc)
 
         serial.write('w'.encode())
-        water = serial.read()
+        waterPeriod = serial.read()
+    
+        if (waterPeriod == b'' or int(waterPeriod) < 0):
+            waterPeriod = b'0'
 
-        if (water == b'' or int(water) < 0):
-            water = b'0'
+        if (currentAdc == b''):
+            currentAdc = b'0'
 
-        if (eletricity == b''):
-            eletricity = b'0'
+        print("WAT:", waterPeriod)
 
-        print("WAT:", water)
+@tl.job(interval=timedelta(seconds=1))
+def sendData(): 
 
+    global currentAdc
+    global waterPeriod
+    global electricity
+    global water
+        
+    if currentAdc is not None and waterPeriod is not None:
+        current = (30*currentAdc) / 1023
+        electricity += 127*current
+        water += 7.5 / waterPeriod
         data = {
-            "electricity" : int(eletricity),
+            "electricity" : int(electricity),
             "water" : int(water)
         }
-
         server.publish(data, "data")
+        print('running')
 
-    
 if __name__ == "__main__":
     main()
