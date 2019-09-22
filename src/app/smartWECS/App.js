425ad2c1867b1146bbe2f4792c7ac4a8378ea6e7
alvaromfcunha <alvaro99.cunha@gmail.com>
2019-09-22 18:27:16 -0300

    app + type errors

---
+++ src/app/smartWECS/App.js
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  Header, 
+} from 'react-native';
+
+
+const App = () => {
+  return (
+      // <Header
+      //   leftComponent={{ icon: 'menu', color: '#fff' }}
+      //   centerComponent={{ text: 'oiS', style: { color: '#fff' } }}
+      //   rightComponent={{ icon: 'home', color: '#fff' }}
+      // />
+      null
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
